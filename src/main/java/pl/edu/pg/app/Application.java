5b24b90693b9a663e0019e54cd5fb24fe7e51ce9
Yooz3k <yooz3k35@gmail.com>
2020-01-03 19:40:24 +0100

    Command line arguments handling

--- src/main/java/pl/edu/pg/app/Application.java
+++ src/main/java/pl/edu/pg/app/Application.java
@@ -1,15 +1,35 @@
 package pl.edu.pg.app;
 
-import java.util.stream.Stream;
+import pl.edu.pg.app.clusters.ClustersFamilyToGraphConverter;
 
 public class Application {
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            Stream.of(args)
-                    .forEach(System.out::println);
-        } else {
-            System.out.println("Hello world!");
+        if (args.length == 0) {
+            System.out.println("Nie wybrano żadnej akcji!");
+            return;
         }
+
+        String firstArg = args[0].toLowerCase();
+        //Pozostałe elementy jako kolejne case'y
+        switch (firstArg) {
+            case "help":
+                System.out.println(getHelp());
+                break;
+            case "-clusters":
+                ClustersFamilyToGraphConverter clusters = new ClustersFamilyToGraphConverter();
+                clusters.execute();
+                break;
+            default:
+                System.out.println("Nie rozpoznano polecenia!");
+                break;
+        }
+    }
+
+    private static String getHelp() {
+        return "Dostępne akcje:\n"
+                + "-clusters: zamiana \"rodziny zgodnych klastrów\" do postaci drzewa\n"
+                //Tutaj proponuję dopisywać info o kolejnych funkcjonalnościach
+                + "...inne opcje...\n";
     }
 }
