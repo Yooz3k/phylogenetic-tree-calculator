0ebc5aef381508c4b8cbdebf6406f18abd5b1ac5
Yooz3k <yooz3k35@gmail.com>
2020-01-04 21:22:58 +0100

    Added checking compatibility of clusters before conversion

--- src/main/java/pl/edu/pg/app/io/GraphLoader.java
+++ src/main/java/pl/edu/pg/app/io/GraphLoader.java
@@ -4,8 +4,16 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
+import pl.edu.pg.app.demos.FileSourceDemo;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GraphLoader {
 
@@ -18,6 +26,7 @@ public class GraphLoader {
             fs.addSink(graph);
             fs.readAll(filepath);
         } catch (IOException e) {
+            System.err.println("Błąd podczas odczytu pliku ze ścieżki " + filepath + ":");
             e.printStackTrace();
         } finally {
             if (fs != null) {
@@ -26,5 +35,31 @@ public class GraphLoader {
         }
 
         return graph;
+    }
+
+    public static List<Graph> loadMultiple(List<String> filenames) {
+        List<String> filepaths = new ArrayList<>();
+        for (String filename : filenames) {
+            String filePath = getFilePath(filename);
+            filepaths.add(filePath);
+        }
+
+        return filepaths.stream()
+                .map(GraphLoader::load)
+                .collect(Collectors.toList());
+    }
+
+    public static String getFilePath(String filename) {
+        URL res = FileSourceDemo.class.getClassLoader().getResource(filename);
+        File file;
+        try {
+            file = Paths.get(res.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            System.err.println("Błąd podczas odczytu pliku o nazwie " + filename + ":");
+            e.printStackTrace();
+            return null;
+        }
+
+        return file.getAbsolutePath();
     }
 }
