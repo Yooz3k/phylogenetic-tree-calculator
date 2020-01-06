2d78477d4f45b63692c76020dad5a21de4006b84
Łukasz Stalmirski <lucasus1129@gmail.com>
2020-01-06 15:59:08 +0100

    Add tree compatibility testing

--- src/main/java/pl/edu/pg/app/clusters/TreeToClustersFamilyConverter.java
+++ src/main/java/pl/edu/pg/app/clusters/TreeToClustersFamilyConverter.java
@@ -5,66 +5,62 @@ import pl.edu.pg.app.converter.GraphToAdjListConverter;
 import pl.edu.pg.app.io.GraphLoader;
 import pl.edu.pg.app.struct.AdjacencyList;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TreeToClustersFamilyConverter {
 
-    private AdjacencyList tree;
+    private AdjacencyList<String> tree;
     private List<String> leaves;
 
     public void convert(String treeFilename) {
         tree = loadTreeFromFile(treeFilename);
 
         Set<Set<String>> clusters = new HashSet<>();
-        leaves = tree.adjacentNodesPerNode.entrySet().stream()
-                .filter(entry -> entry.getValue().isEmpty())
-                .map(Map.Entry::getKey)
+        leaves = tree.GetNodes().stream()
+                .filter( node -> tree.GetNodeEdges( node ).isEmpty() )
                 .collect(Collectors.toList());
 
-        tree.adjacentNodesPerNode.forEach((keyNode, adjacentNodes) -> {
+        tree.GetNodes().forEach( keyNode -> {
+            List<String> adjacentNodes = tree.GetNodeEdges( keyNode );
             //Jeżeli wierzchołek jest liściem, jest on dodawany jako jednoelementowa kolekcja do rodziny
             if (adjacentNodes.isEmpty()) {
                 clusters.add(Collections.singleton(keyNode));
             } else {
                 //Zbieramy wierzchołki, które są liśćmi
-                Set<String> currentNodeLeaves = adjacentNodes.stream()
+                List<String> currentNodeLeaves = adjacentNodes.stream()
                         .filter(node -> leaves.contains(node))
-                        .collect(Collectors.toSet());
+                        .collect(Collectors.toList());
 
                 //Następnie wierzchołki, które nie są liśćmi
-                Set<String> notLeafNodes = adjacentNodes.stream()
+                List<String> notLeafNodes = adjacentNodes.stream()
                         .filter(node -> !leaves.contains(node))
-                        .collect(Collectors.toSet());
+                        .collect(Collectors.toList());
 
                 //Dla wszystkich tych, które nie są liśćmi, musimy znaleźć ich wszystkie liście
                 currentNodeLeaves.addAll(getLeaves(notLeafNodes));
 
-                clusters.add(currentNodeLeaves);
+                clusters.add(Set.copyOf( currentNodeLeaves ));
             }
         });
 
         System.out.println(clusters);
     }
 
-    private Set<String> getLeaves(Set<String> adjacentNodes) {
+    private Set<String> getLeaves(List<String> adjacentNodes) {
         Set<String> currentLeaves = new HashSet<>(4);
         adjacentNodes.forEach(adjNode -> {
             if (leaves.contains(adjNode)) {
                 currentLeaves.add(adjNode);
             } else {
                 //Rekurencyjnie "zbieramy" wszystkie liście dla kolejnych węzłów, które liśćmi nie są.
-                currentLeaves.addAll(getLeaves(tree.get(adjNode)));
+                currentLeaves.addAll(getLeaves(tree.GetNodeEdges(adjNode)));
             }
         });
         return currentLeaves;
     }
 
-    private AdjacencyList loadTreeFromFile(String filename) {
+    private AdjacencyList<String> loadTreeFromFile(String filename) {
         String path = GraphLoader.getFilePath(filename);
         Graph graph = GraphLoader.load(path);
         //graph.display();
---
+++ src/main/java/pl/edu/pg/app/compatibility/CompatibilityFinder.java
@@ -0,0 +1,177 @@
+package pl.edu.pg.app.compatibility;
+
+import pl.edu.pg.app.converter.ClusterToGraphConverter;
+import pl.edu.pg.app.converter.GraphToClusterConverter;
+import pl.edu.pg.app.io.GraphLoader;
+import pl.edu.pg.app.struct.AdjacencyList;
+import pl.edu.pg.app.struct.Cluster;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CompatibilityFinder
+{
+    private Random m_Random = new Random();
+
+    public static void Execute( List<String> arguments )
+    {
+        CompatibilityFinder compatibilityFinder = new CompatibilityFinder();
+
+        // Process arguments and return unparsed list
+        arguments = compatibilityFinder.ParseArguments( arguments );
+
+        GraphToClusterConverter graphConverter = new GraphToClusterConverter();
+        List<Cluster> trees = arguments.parallelStream()
+                .map( GraphLoader::load )
+                .map( graphConverter::convert )
+                .collect( Collectors.toList() );
+
+        List<List<Cluster>> treeClusters = trees.stream()
+                .map( Cluster::GetAllClusters )
+                .collect( Collectors.toList() );
+
+        Cluster compatibleTree = compatibilityFinder.FindCompatibleTree( trees, treeClusters );
+
+        if( compatibleTree != null )
+        {
+            System.out.println( "Compatible tree: " + compatibleTree );
+
+            // Display compatible tree
+            new ClusterToGraphConverter().Convert( compatibleTree ).display();
+        }
+        else
+        {
+            System.out.println( "Trees incompatible" );
+        }
+    }
+
+    private List<String> ParseArguments( List<String> arguments )
+    {
+        return arguments;
+    }
+
+    private Cluster FindCompatibleTree( List<Cluster> nodes, List<List<Cluster>> treeClusters )
+    {
+        // Implementation of BuildST from
+        // https://arxiv.org/pdf/1510.07758.pdf
+
+        // Cover simple cases - single-species trees and two-species trees
+        List<String> uniqueTerminals = nodes.stream()
+                .flatMap( Cluster::GetTerminalsStream )
+                .distinct()
+                .collect( Collectors.toList() );
+
+        if( uniqueTerminals.size() == 1 )
+        {
+            return new Cluster( m_Random.nextInt(), uniqueTerminals.get( 0 ) );
+        }
+
+        if( uniqueTerminals.size() == 2 )
+        {
+            return new Cluster( m_Random.nextInt(),
+                    new Cluster( m_Random.nextInt(), uniqueTerminals.get( 0 ) ),
+                    new Cluster( m_Random.nextInt(), uniqueTerminals.get( 1 ) ) );
+        }
+
+        List<Cluster> newNodes = new ArrayList<>();
+
+        for( List<Cluster> tree : treeClusters )
+        {
+            List<Cluster> intersection = IntersectClusters( nodes, tree );
+
+            if( intersection.size() == 1 )
+            {
+                // Remove singletons by replacing them with their children
+                newNodes.addAll( intersection.get( 0 ).GetClusters() );
+            }
+            else
+            {
+                // Leave unchanged
+                newNodes.addAll( intersection );
+            }
+        }
+
+        // Get connected components of the new node set
+        List<List<Cluster>> connectedComponents = GetConnectedComponents( newNodes );
+
+        if( connectedComponents.size() == 1 )
+        {
+            // If newNodes subset is still fully connected, the tree is incompatible
+            return null;
+        }
+
+        Cluster root = new Cluster( m_Random.nextInt() );
+
+        for( List<Cluster> component : connectedComponents )
+        {
+            // Call recursively for each connected component
+            Cluster subtreeCompatibilityTree = FindCompatibleTree( component, treeClusters );
+
+            if( subtreeCompatibilityTree != null )
+            {
+                root.Add( subtreeCompatibilityTree );
+            }
+            else
+            {
+                // If any of subtrees is incompatible, the tree is incompatible
+                return null;
+            }
+        }
+
+        return root;
+    }
+
+    private List<List<Cluster>> GetConnectedComponents( List<Cluster> nodes )
+    {
+        // Create the graph where nodes are connected if both lead to one or more common terminals
+        AdjacencyList<Cluster> relatedClusters = new AdjacencyList<>();
+
+        nodes.forEach( relatedClusters::AddNode );
+
+        for( Cluster node0 : nodes )
+        {
+            for( Cluster node1 : nodes )
+            {
+                if( node0 == node1 )
+                {
+                    continue;
+                }
+
+                if( AnyIntersects( node0.GetTerminals(), node1.GetTerminals() ) )
+                {
+                    relatedClusters.AddEdge( node0, node1 );
+                }
+            }
+        }
+
+        // Extract components from the created graph
+        List<List<Cluster>> connectedComponents = new ArrayList<>();
+
+        while( !relatedClusters.Empty() )
+        {
+            List<Cluster> clusters = relatedClusters.GetNodes();
+            List<Cluster> visitedClusters = relatedClusters.CollectInOrder( clusters.get( 0 ) );
+
+            // Include source cluster in the visited set
+            assert( visitedClusters.contains( clusters.get( 0 ) ) );
+
+            connectedComponents.add( visitedClusters );
+
+            visitedClusters.forEach( relatedClusters::RemoveNode );
+        }
+
+        return connectedComponents;
+    }
+
+    private List<Cluster> IntersectClusters( List<Cluster> a, List<Cluster> b )
+    {
+        return a.stream()
+                .filter( aCluster -> b.stream().anyMatch( bCluster -> aCluster.GetId() == bCluster.GetId() ) )
+                .collect( Collectors.toList() );
+    }
+
+    private <T> boolean AnyIntersects( List<T> a, List<T> b )
+    {
+        return a.stream().anyMatch( b::contains );
+    }
+}
